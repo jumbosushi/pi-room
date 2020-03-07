b0f72567ef8214823c75133257ecc0e2a1586c80
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2020-03-06 23:50:36 -0800

    Add code

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"./sensors"
+
+	"context"
+	"log"
+	"time"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+// RoomData is a struct for storing room data
+type RoomData struct {
+	temp     float64
+	humidity float64
+	pressure float64
+	light    float64
+}
+
+func recordData(roomData *RoomData) {
+	ctx := context.Background()
+	// TODO: Change to your key file
+	opt := option.WithCredentialsFile("~/pi-room-key.json")
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	collection := client.Collection("conditions")
+
+	_, _, err = collection.Add(ctx, map[string]interface{}{
+		"createdAt":   time.Now(),
+		"temperature": roomData.temperature,
+		"humidity":    roomData.humidity,
+		"pressure":    roomData.pressure,
+		"light":       roomData.light,
+	})
+	if err != nil {
+		log.Fatalf("Failed adding document: %v", err)
+	}
+}
+
+func main() {
+	log.Println("Starting pi-room ...")
+
+	temperature1, _ := sensors.GetTemp()
+	log.Printf("Temperature:  %f C\n", temperature1)
+
+	temperature1, _ := sensors.GetHumidity()
+	log.Printf("Humidity:     %f %%rh\n", humidity)
+
+	light, _ := sensors.GetLight()
+	log.Printf("Light:        %f Lux\n", light)
+
+	pressure, _ := sensors.GetTempAndPressure()
+	log.Printf("Pressure:     %f %%rh\n", pressure)
+
+	data := RoomData{
+		temperature: temperature1,
+		pressure:    pressure,
+		humidity:    humidity,
+		light:       light,
+	}
+
+	recordData(&data)
+	log.Println("Successfully recorded data!")
+}
---
+++ sensors/sensors.go
@@ -0,0 +1,84 @@
+package sensors
+
+import (
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+// GetTemp returns the temperature
+func GetTemp() (temp float64, err error) {
+	out, err := exec.Command("/home/pi/anavi-examples/sensors/BMP180/c/BMP180").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := string(out)
+	lineTemp := strings.Split(s, "\n")[1]
+	tempStr := strings.Fields(lineTemp)[1]
+
+	temp, err = strconv.ParseFloat(tempStr, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return
+}
+
+// GetHumidity returns the humidity
+func GetHumidity() (humidity float64, err error) {
+	out, err := exec.Command("/home/pi/anavi-examples/sensors/HTU21D/c/HTU21D").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := string(out)
+	lineHum := strings.Split(s, "\n")[2]
+	humidityStr := lineHum[:len(lineHum)-3]
+	humidity, err = strconv.ParseFloat(humidityStr, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return
+}
+
+// GetPressure returns the pressure
+func GetPressure() (pressure float64, err error) {
+	out, err := exec.Command("/home/pi/anavi-examples/sensors/BMP180/c/BMP180").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := string(out)
+	linePress := strings.Split(s, "\n")[2]
+	pressureStr := strings.Fields(linePress)[1]
+
+	pressure, err = strconv.ParseFloat(pressureStr, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return
+}
+
+// GetLight returns the light
+func GetLight() (light float64, err error) {
+	out, err := exec.Command("/home/pi/anavi-examples/sensors/BH1750/c/BH1750").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := string(out)
+	lineLight := strings.Split(s, "\n")[1]
+	lightStr := strings.Fields(lineLight)[1]
+
+	light, err = strconv.ParseFloat(lightStr, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return
+
+}
