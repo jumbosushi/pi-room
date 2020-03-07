82dfcf9ab916aa6a4ba89aa44751dbd2ec1e4cf3
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2020-03-06 23:51:17 -0800

    Change the wording for TODO

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ type RoomData struct {
 
 func recordData(roomData *RoomData) {
 	ctx := context.Background()
-	// TODO: Change to your key file
+	// TODO: Change the path to your key file
 	opt := option.WithCredentialsFile("~/pi-room-key.json")
 	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
